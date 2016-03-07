8db7556e85ec579e3a19ea81388a1235c14c5ea5
houzhiwei <houzhiwei@allinmd.cn>
2016-03-07 20:05:57 +0800

    AFNetworking 3.0
     Add  AFNetworking 3.0

--- Masonry111/Pods/Target Support Files/Pods-Masonry111/Pods-Masonry111-frameworks.sh
+++ Masonry111/Pods/Target Support Files/Pods-Masonry111/Pods-Masonry111-frameworks.sh
@@ -84,8 +84,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-Masonry111/AFNetworking.framework"
   install_framework "Pods-Masonry111/Masonry.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-Masonry111/AFNetworking.framework"
   install_framework "Pods-Masonry111/Masonry.framework"
 fi
